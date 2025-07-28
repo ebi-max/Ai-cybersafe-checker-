01694f39eda5a8ae96ae29564d034f2d53a44d6d
ebi-max <ebiemebassey887@gmail.com>
2025-07-28 21:01:30 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import requests
-import streamlit_authenticator as stauth
+import streamlit-authenticator as stauth
 import pandas as pd
 import gspread
 from google.oauth2.service_account import Credentials
